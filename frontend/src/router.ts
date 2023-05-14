d87916b86638ef37a312a6be84b7bd372507a312
Vlad Kramarukha <vald1sl0ve.00@gmail.com>
2023-05-14 16:26:56 +0400

    Сделал изменяемый title

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useTitle } from '@vueuse/core'
 
+type Title = string
 export enum Routes {
 	HomePage = '/',
 	LogPage = '/login',
 	RegPage = '/registration'
 }
 
+const title = useTitle()
 const useLoginPage = () => import('./pages/LoginPage.vue')
 
 const router = createRouter({
@@ -14,18 +17,32 @@ const router = createRouter({
 		{
 			path: Routes.HomePage,
 			redirect: Routes.LogPage,
+			meta: {
+				title: 'Главная страница'
+			}
 		},
 		{
 			name: 'log',
 			path: Routes.LogPage,
-			component: useLoginPage
+			component: useLoginPage,
+			meta: {
+				title: 'Вход'
+			}
 		},
 		{
 			name: 'reg',
 			path: Routes.RegPage,
-			component: useLoginPage
+			component: useLoginPage,
+			meta: {
+				title: 'Регистрация'
+			}
 		},
 	],
+})
+
+router.beforeEach((to, from, next) => {
+	title.value = to.meta.title as Title
+	next()
 })
 
 export default router
