3d20bb2b867291bb8fac2e6eba2a5ab34aaa636f
Vlad Kramarukha <vald1sl0ve.00@gmail.com>
2023-05-21 15:30:32 +0400

    Добавил верстку для страницы админа и сотрудника

--- frontend/src/pages/AdminPage.vue
+++ frontend/src/pages/AdminPage.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-
+	<div class="h-screen grid place-content-center">
+		<h1>АДМИНИСТРИРОВАНИЕ</h1>
+	</div>
 </template>
 
 <style scoped>
--- frontend/src/pages/EmployeePage.vue
+++ frontend/src/pages/EmployeePage.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-
+	<div class="h-screen grid place-content-center">
+		<h1>СИСТЕМА</h1>
+	</div>
 </template>
 
 <style scoped>
